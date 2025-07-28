740b344f0a08c43f3e2aed37fb3de593432b10c0
Prasoon <senpailoveshentai.uwu@gmail.com>
2025-07-28 21:49:06 +0530

    Bump version to 0.1.1

--- mlgraphy/__init__.py
+++ mlgraphy/__init__.py
@@ -7,5 +7,5 @@ with automatic detection of validation data and clean, publication-ready plots.
 
 from .plotting import plot_metrics
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["plot_metrics"]
